05a03198287708e5725e9dd09b8189416cc7bfbe
Dhairy Srivastava <dhairysrivastava5@gmail.com>
2024-11-20 10:55:15 +0530

    Add Config.ts

--- build.ts
+++ build.ts
@@ -2,8 +2,7 @@ import handlerbars from "npm:handlebars";
 import { render } from "@deno/gfm";
 import { extractYaml } from "jsr:@std/front-matter";
 import ogImageGenerator from "./og.ts";
-
-const domain = "https://nightly-blog.deno.dev";
+import { domain } from "./config.ts";
 const index = Deno.readTextFileSync("./index.html");
 const cssPath = Deno.readTextFileSync("./new.css");
 
