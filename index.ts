a61dfdb63c6aa7194f4255789d950d8bf5f421ad
Dhairy Srivastava <dhairysrivastava5@gmail.com>
2024-11-20 10:16:32 +0530

    Fix

--- index.ts
+++ index.ts
@@ -1,20 +1,5 @@
 import { contentType } from "jsr:@std/media-types/content-type";
 
-function readFile(path: string) {
-  const file = Deno.readTextFileSync(path);
-  return file;
-}
-function contentType(path: string) {
-  if (path.endsWith(".html")) {
-    return "text/html";
-  } else if (path.endsWith(".css")) {
-    return "text/css";
-  } else if (path.endsWith(".js")) {
-    return "text/javascript";
-  } else {
-    return "text/plain";
-  }
-}
 Deno.serve((req: Request) => {
   const url = new URL(req.url);
   let path = url.pathname;
@@ -22,8 +7,10 @@ Deno.serve((req: Request) => {
     path = "index.html";
   }
   // type of file
-  const type = contentType(path);
-  const file = readFile(`./out/${path}`);
+  const pathExtention = path.split(".")[1];
+  const type = contentType(pathExtention)!;
+
+  const file = Deno.readFileSync(`./out/${path}`);
 
   return new Response(file, {
     headers: {
